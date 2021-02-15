ee2618c121e8be316e4a2840a66bc7715012d2f0
Andy Tran <hello@andytran.me>
2021-02-15 14:19:07 -0800

    fix ssr for stitches

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,25 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
 
 import { getCssString } from '~/stitches.config';
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    try {
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            <style id='stitches'>{getCssString()}</style>
+          </>
+        ),
+      };
+    } finally {
+    }
+  }
+
   render() {
     return (
       <Html lang='en'>
@@ -21,7 +38,6 @@ export default class MyDocument extends Document {
           <link href='/images/icons/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
 
           <link rel='apple-touch-icon' href='/images/icons/apple-icon.png'></link>
-          <style>{getCssString()}</style>
         </Head>
         <body>
           <Main />
