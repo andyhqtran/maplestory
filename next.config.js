ed8c47ff54cbc0732ab27a40ca81565b224f4790
Andy Tran <hello@andytran.me>
2021-02-15 14:18:57 -0800

    add in image domain for graphcms

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
   future: { webpack5: true },
+  images: {
+    domains: ['media.graphcms.com'],
+  },
   pwa: {
     dest: 'public',
     runtimeCaching,
