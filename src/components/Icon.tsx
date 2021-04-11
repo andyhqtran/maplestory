2f221dee6cebee97fa665f005b08e8ce420f4f35
Andy Tran <hello@andytran.me>
2021-04-11 04:36:11 -0700

    add in export icon

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -59,6 +59,17 @@ export const DataIcon = (props: DataIconProps) => {
   );
 };
 
+export type ExportIconProps = IconProps;
+
+export const ExportIcon = (props: ExportIconProps) => {
+  return (
+    <Icon viewBox='0 0 24 24' {...props}>
+      <path d='M11 16L13 16 13 7 16 7 12 2 8 7 11 7z' />
+      <path d='M5,22h14c1.103,0,2-0.897,2-2v-9c0-1.103-0.897-2-2-2h-4v2h4v9H5v-9h4V9H5c-1.103,0-2,0.897-2,2v9C3,21.103,3.897,22,5,22 z' />
+    </Icon>
+  );
+};
+
 export type ListIconProps = IconProps;
 
 export const ListIcon = (props: ListIconProps) => {
