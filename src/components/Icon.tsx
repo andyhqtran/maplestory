2da3ea2d610f541785078dae424df4071fd54552
Andy Tran <hello@andytran.me>
2021-04-11 03:14:44 -0700

    update button sizes

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -20,9 +20,13 @@ export const Icon = styled('svg', {
         width: 20,
         height: 20,
       },
-      'extra-small': {
+      tiny: {
         width: 16,
         height: 16,
+      },
+      micro: {
+        width: 12,
+        height: 12,
       },
     },
   },
@@ -102,6 +106,16 @@ export const PencilIcon = (props: PencilIconProps) => {
   return (
     <Icon viewBox='0 0 24 24' {...props}>
       <path d='M8.707 19.707L18 10.414 13.586 6l-9.293 9.293c-.128.128-.219.289-.263.464L3 21l5.242-1.03C8.418 19.926 8.579 19.835 8.707 19.707zM21 7.414c.781-.781.781-2.047 0-2.828L19.414 3c-.781-.781-2.047-.781-2.828 0L15 4.586 19.414 9 21 7.414z' />
+    </Icon>
+  );
+};
+
+export type PlusIconProps = IconProps;
+
+export const PlusIcon = (props: PlusIconProps) => {
+  return (
+    <Icon viewBox='0 0 24 24' {...props}>
+      <path d='M19 11L13 11 13 5 11 5 11 11 5 11 5 13 11 13 11 19 13 19 13 13 19 13z' />
     </Icon>
   );
 };
